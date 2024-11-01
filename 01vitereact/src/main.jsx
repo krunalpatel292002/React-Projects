10cb83ab744e32af1184146831f1bef3830f029d
Krunalpatel292002 <patelkeunal292002@gmail.com>
2024-11-01 18:25:29 +0530

    push 2nd Project

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | chAI</h1>
+    </div>
+  )
+}
+
+// const ReactElement= {
+//   type: "a",
+//   props :{
+//       href: "https:www.google.com",
+//       target: "_blank"
+//   },
+//   children: "Click Here To Visit."
+
+// }
+
+const anotherElement = (
+  <a href="https:www.google.com" target= "_blank">"Click Here To Visit."</a>
+)
+
+const ReactElement = React.createElement(
+  'a',
+  {
+    href:"https:www.google.com",
+    target: "_blank"
+  },
+  "Click Here To Visit."
+
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  ReactElement
+  
 )
